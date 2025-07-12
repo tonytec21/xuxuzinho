6e26d0524e189188dbee19398744c8666a43fbb5
Tony Tec <88214774+tonytec21@users.noreply.github.com>
2025-07-12 16:54:29 -0300

    Update relatorio-inventario-bens.php

--- relatorio-inventario-bens.php
+++ relatorio-inventario-bens.php
@@ -173,7 +173,7 @@ $pdf->Ln(1);
 foreach ($tipos as $tipo){
     $pdf->SetFont('helvetica','B',13);
     $pdf->Cell(0,8,'Tipo: '.safeText($tipo['nome']),0,1,'L');
-    $pdf->Ln(2);
+    $pdf->Ln(1);
 
     $catStmt = $pdo->prepare("
         SELECT id,nome
