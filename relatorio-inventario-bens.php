55be544e584ac0cedcd69d2a6574dda5ff1ea7b0
Tony Tec <88214774+tonytec21@users.noreply.github.com>
2025-07-12 14:44:51 -0300

    Update relatorio-inventario-bens.php

--- relatorio-inventario-bens.php
+++ relatorio-inventario-bens.php
@@ -145,30 +145,30 @@ $pdf->Cell(0,8,'Gerado em: '.date('d/m/Y H:i'),0,1,'C');
 $pdf->Ln(2);
 
 /* cards */
-$htmlCards = '<table cellspacing="5" cellpadding="5" width="100%"><tr>';
-$c=0;
-foreach ($cards as $nome=>$valor){
-    $cor = $cardColors[$nome];
-    if ($c && $c%3==0) $htmlCards.='</tr><tr>';
-    $htmlCards .= '
-      <td width="33%" style="
-          background:'.$cor.';
-          color:#fff;
-          border-radius:10px;
-          text-align:center;">
-        <div style="font-size:10px;font-weight:bold">'.mb_strtoupper($nome,'UTF-8').'</div>
-        <div style="font-size:18px;font-weight:bold">'.$valor.'</div>
-      </td>';
-    $c++;
-}
-if ($c%3) $htmlCards .= str_repeat('<td width="33%"></td>', 3-($c%3));
-$htmlCards .= '</tr></table><br>';
-$pdf->writeHTML($htmlCards,true,false,true,false,'');
+// $htmlCards = '<table cellspacing="5" cellpadding="5" width="100%"><tr>';
+// $c=0;
+// foreach ($cards as $nome=>$valor){
+//     $cor = $cardColors[$nome];
+//     if ($c && $c%3==0) $htmlCards.='</tr><tr>';
+//     $htmlCards .= '
+//       <td width="33%" style="
+//           background:'.$cor.';
+//           color:#fff;
+//           border-radius:10px;
+//           text-align:center;">
+//         <div style="font-size:10px;font-weight:bold">'.mb_strtoupper($nome,'UTF-8').'</div>
+//         <div style="font-size:18px;font-weight:bold">'.$valor.'</div>
+//       </td>';
+//     $c++;
+// }
+// if ($c%3) $htmlCards .= str_repeat('<td width="33%"></td>', 3-($c%3));
+// $htmlCards .= '</tr></table><br>';
+// $pdf->writeHTML($htmlCards,true,false,true,false,'');
 
 /* tipos / categorias / bens ---------------------------------------- */
 foreach ($tipos as $tipo){
     $pdf->SetFont('helvetica','B',14);
-    $pdf->Cell(0,8,'Tipo: '.$tipo['nome'],0,1,'L');
+    $pdf->Cell(0,8,'TIPO: '.$tipo['nome'],0,1,'L');
     $pdf->Ln(2);
 
     $catStmt = $pdo->prepare(
