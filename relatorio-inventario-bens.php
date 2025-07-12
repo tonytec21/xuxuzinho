db2ed63e015e320fe6a242c87e4dec3d9da5b052
Tony Tec <88214774+tonytec21@users.noreply.github.com>
2025-07-12 17:50:02 -0300

    Update relatorio-inventario-bens.php

--- relatorio-inventario-bens.php
+++ relatorio-inventario-bens.php
@@ -171,7 +171,7 @@ $pdf->Ln(1);
 
 /* ----------- DETALHAMENTO ---------------------------------------- */
 foreach ($tipos as $tipo){
-    $pdf->Ln(3);
+    $pdf->Ln(5);
     $pdf->SetFont('helvetica','B',13);
     $pdf->Cell(0,8,'Tipo: '.safeText($tipo['nome']),0,1,'L');
     $pdf->Ln(0);
